7e2884c743bc8a63a6e2cd109b9d7376732f3fff
59024077 <pat_l2009@hotmail.com>
2020-01-14 09:06:00 +0700

    gg

--- controller_record_ma.js
+++ controller_record_ma.js
@@ -1,7 +1,14 @@
-$('#save_date').daterangepicker({
+$('#ma_date').daterangepicker({
     singleDatePicker: true,
     showDropdowns: true,
     locale: {
         format: 'DD/MM/YYYY'
+    },
+});
+$('#ma_date_year').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    locale: {
+        format: "yyyy"
     },
 });
