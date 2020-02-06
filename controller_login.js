e2aaeff70b1048407c3f5c664c91d676606b2e6a
59024077 <pat_l2009@hotmail.com>
2020-02-06 16:51:01 +0700

    เพิ่ม login

--- controller_login.js
+++ controller_login.js
@@ -1,6 +1,6 @@
 function lo_log() {
-    var username = $('#username').val();
-    var password = $('#password').val();
+    var username = $("#username").val();
+    var password = $("#password").val();
     
     console.log(username);
     console.log(password);
@@ -26,8 +26,8 @@ function lo_log() {
             } else {
 
                 Swal.fire({
-                    icon: 'error',
-                    title: 'แก้ไขข้อมูลไม่สำเร็จ',
+                    type: 'error',
+                    title: 'กรุณากรอกรหัสผ่าน',
                     showConfirmButton: false,
                     timer: 1500
                 })
@@ -61,35 +61,35 @@ function lo_log() {
 
 
 
-function showlogin() {
-    var username = $("#username").val();
-    var password = $("#password").val();
-    $.ajax({
-        async: true,
-        url: "php_group/action.php?en=showlogindata",
-        type: "POST",
-        data: {
-            username: username,
-            password: password
+// function showlogin() {
+//     var username = $("#username").val();
+//     var password = $("#password").val();
+//     $.ajax({
+//         async: true,
+//         url: "php_group/action.php?en=showlogindata",
+//         type: "POST",
+//         data: {
+//             username: username,
+//             password: password
 
-        },
-        success: function (data) {
-            if (data != 0) {
-                var dataJSON = JSON.parse(data);
-                console.log(dataJSON);
-                $("#eid").val(dataJSON.eid);
-                $("#finger_id").val(dataJSON.finger_id);
-                $("#prefix").val(dataJSON.prefix);
-                $("#first_name").val(dataJSON.first_name);
-                $("#last_names").val(dataJSON.last_names);
-                $("#nickname").val(dataJSON.nickname);
-                $("#department_id").val(dataJSON.department_id);
-                $("#working_group").val(dataJSON.working_group);
-                $("#position").val(dataJSON.position);
+//         },
+//         success: function (data) {
+//             if (data != 0) {
+//                 var dataJSON = JSON.parse(data);
+//                 console.log(dataJSON);
+//                 $("#eid").val(dataJSON.eid);
+//                 $("#finger_id").val(dataJSON.finger_id);
+//                 $("#prefix").val(dataJSON.prefix);
+//                 $("#first_name").val(dataJSON.first_name);
+//                 $("#last_names").val(dataJSON.last_names);
+//                 $("#nickname").val(dataJSON.nickname);
+//                 $("#department_id").val(dataJSON.department_id);
+//                 $("#working_group").val(dataJSON.working_group);
+//                 $("#position").val(dataJSON.position);
 
-            }
-        }
+//             }
+//         }
 
 
-    });
-}
+//     });
+// }
